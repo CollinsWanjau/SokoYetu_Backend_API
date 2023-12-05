22ae40659fa05ab85ff41d85a7f22622e2f68acd
colloso999 <tripperskripper@gmail.com>
2023-12-05 11:08:59 +0300

    Creaed a refresh token

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -2,7 +2,7 @@ const { generateToken } = require('../config/jwtToken');
 const User = require('../models/userModel');
 const asyncHandler = require('express-async-handler')
 const validateMongoDbId = require('../utils/validateMongoDbId')
-
+const generateRefreshToken = require('../config/refreshToken')
 /**
  * createUser - Async function to create a new user.
  *
@@ -46,6 +46,19 @@ const loginUserCtrl = asyncHandler(async (req, res) => {
 
   // Validate the user's password
   if(findUser && (await findUser.isPasswordMatched(password))) {
+
+    const refreshToken = await generateRefreshToken(findUser?._id)
+    const updateUser = await User.findByIdAndUpdate(
+      findUser.id,
+      {
+        refreshToken: refreshToken,
+      },
+      { new: true }
+    )
+    res.cookie('refreshToken', refreshToken, {
+      httpOnly: true,
+      maxAge:72 * 60 * 60 * 1000
+    })
     // If the password is correct, send a JSON response with user information and a token
     res.json({
       _id: findUser?._id,
