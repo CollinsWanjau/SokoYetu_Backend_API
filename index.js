1840a975298e49b95b33e1f2bc5c9d4442f8505f
colloso999 <tripperskripper@gmail.com>
2023-12-05 11:12:04 +0300

    Creaed a cookie-parser middle-ware function that parses the cookie header

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const dotenv = require('dotenv').config()
 const PORT = process.env.PORT || 4000
 const authRoute = require('./routes/authRoutes')
 const { notFound, errorHandler } = require('./middlewares/errorHandler')
+const cookieParser = require('cookie-parser')
 // const bodyParser = require('body-parser')
 connectDB()
 
@@ -15,7 +16,9 @@ connectDB()
 // app.use(bodyParser.urlencoded({ extended: false}))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false}))
+app.use(cookieParser())
 app.use('/api/user', authRoute)
+
 
 // after authroute we pass in the middleware
 app.use(notFound)
