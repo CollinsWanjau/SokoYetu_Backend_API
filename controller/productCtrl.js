4ec837e05b0f37c80283e22c300a7937c5b3022c
colloso999 <tripperskripper@gmail.com>
2023-12-06 10:24:07 +0300

    Add slug generation for product title and implement update and delete functionality

--- controller/productCtrl.js
+++ controller/productCtrl.js
@@ -1,8 +1,14 @@
 const Product = require('../models/productModel');
 const asyncHandler = require('express-async-handler');
-
+const slugify = require('slugify')
 const createProduct = asyncHandler(async (req, res) => {
   try {
+    if (req.body.title) {
+        req.body.slug = slugify(req.body.title)
+    }
+    if (!req.body.slug) {
+      throw new Error('Failed to generate a valid slug from the provided title.');
+    }
     const newProduct = await Product.create(req.body)
     res.json(newProduct);
   } catch (error) {
@@ -10,6 +16,33 @@ const createProduct = asyncHandler(async (req, res) => {
   }
 });
 
+const updateProduct = asyncHandler(async (req, res) => {
+    // const id = req.params
+    const productId = req.params.id;
+    try {
+        if (req.body.title) {
+            req.body.slug = slugify(req.body.title)
+        }
+        const updateProduct = await Product.findOneAndUpdate({_id: productId}, req.body, {
+            new: true
+        })
+        res.json(updateProduct)
+    } catch(error) {
+        throw new Error(error)
+    }  
+})
+
+const deleteProduct = asyncHandler(async (req, res) => {
+    // const id = req.params
+    const productId = req.params.id;
+    try {
+        
+        const deleteProduct = await Product.findOneAndDelete({_id: productId})
+        res.json(deleteProduct)
+    } catch(error) {
+        throw new Error(error)
+    }  
+})
 const getaProduct = asyncHandler(async (req, res) => {
     const {id} = req.params
     try {
@@ -28,4 +61,6 @@ const getAllProduct = asyncHandler(async (req, res) => {
         throw new Error(error)
     }
 })
-module.exports = { createProduct, getaProduct, getAllProduct};
+
+
+module.exports = { createProduct, getaProduct, getAllProduct, updateProduct, deleteProduct};
