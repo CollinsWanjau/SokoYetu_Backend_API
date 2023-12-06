aa02108e6dad84c5ed6a022a98d49f2c01ea0050
colloso999 <tripperskripper@gmail.com>
2023-12-06 13:32:39 +0300

    Add filtering, sorting, limiting fields, and pagination to getAllProduct endpoint

--- controller/productCtrl.js
+++ controller/productCtrl.js
@@ -62,6 +62,7 @@ const getaProduct = asyncHandler(async (req, res) => {
 const getAllProduct = asyncHandler(async (req, res) => {
     // console.log(req.query)
     try {
+        // Filtering
         // Extract query parameters from the request
         const queryObj = {...req.query}
 
@@ -74,7 +75,38 @@ const getAllProduct = asyncHandler(async (req, res) => {
         queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`)
 
         // Parse the modified query string and execute the query
-        const query = Product.find(JSON.parse(queryStr))
+        let query = Product.find(JSON.parse(queryStr))
+
+        // Sorting
+
+        if (req.query.sort) {
+            const sortBy = req.query.sort.split(',').join(' ')
+            query = query.sort(sortBy)
+        } else {
+            query = query.sort('-createdAt')
+        }
+
+        // limiting fields
+        if (req.query.fields) {
+            const fields = req.query.fields.split(',').join(' ')
+            query = query.select(fields)
+        } else {
+            query.select('-__v')
+        }
+
+        // Pagination
+
+        const page = req.query.page
+        const limit = req.query.limit
+        const skip = (page - 1) * limit
+        query = query.skip(skip).limit(limit)
+        if(req.query.page) {
+            const productCount = await Product.countDocuments()
+            if(skip >= productCount) throw new Error('This page does not exist')
+        }
+        console.log(page, limit, skip)
+
+
         const product = await query
 
         // Send the resulting products as a JSON response
