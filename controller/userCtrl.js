c7d997869a876b816e568b2d7306dd376e35a8f6
colloso999 <tripperskripper@gmail.com>
2023-12-11 16:35:51 +0300

    Generate a password token for the forgot password functionality

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -1,9 +1,9 @@
 const { generateToken } = require('../config/jwtToken');
 const User = require('../models/userModel');
-const asyncHandler = require('express-async-handler')
-const validateMongoId = require('../utils/validateMongoId')
-const { generateRefreshToken } = require('../config/refreshToken')
-const jwt = require('jsonwebtoken')
+const asyncHandler = require('express-async-handler');
+const validateMongoId = require('../utils/validateMongoId');
+const { generateRefreshToken } = require('../config/refreshToken');
+const jwt = require('jsonwebtoken');
 /**
  * createUser - Async function to create a new user.
  *
@@ -17,18 +17,17 @@ const createUser = asyncHandler(async (req, res) => {
   const email = req.body.email;
 
   // Check if a user with the provided email already exists
-  const findUser = await User.findOne({email: email});
+  const findUser = await User.findOne({ email });
 
   if (!findUser) {
     // If user doesn't exist, create a new User
     const newUser = new User(req.body);
     await newUser.save(); // Save the new user to the database
-    res.json(newUser);  // Send the new user as a JSON response
+    res.json(newUser); // Send the new user as a JSON response
   } else {
-    throw new Error('User already exists')
+    throw new Error('User already exists');
   }
 });
-
 
 /**
  * Controller function for user login.
@@ -38,35 +37,33 @@ const createUser = asyncHandler(async (req, res) => {
  * @returns {Promise<void>} - A Promise that resolves when the function is complete.
  */
 const loginUserCtrl = asyncHandler(async (req, res) => {
-
   // Extract email and password from the request body
-  const {email, password} = req.body
+  const { email, password } = req.body;
 
   // check if user exists
-  const findUser = await User.findOne({ email })
+  const findUser = await User.findOne({ email });
 
   // Validate the user's password
-  if(findUser && (await findUser.isPasswordMatched(password))) {
-
-    // This function creates a unique token that can be used to obtain new 
-    // access tokens when the old ones expire. The function returns the 
+  if (findUser && (await findUser.isPasswordMatched(password))) {
+    // This function creates a unique token that can be used to obtain new
+    // access tokens when the old ones expire. The function returns the
     // refresh token as a promise
-    const refreshToken = await generateRefreshToken(findUser?._id)
+    const refreshToken = await generateRefreshToken(findUser?._id);
 
     // a way to update a document in a MongoDB database.
     // It passes the user’s ID, the new refresh token, and an option to return the updated document as arguments
     const updateUser = await User.findByIdAndUpdate(
       findUser.id,
       {
-        refreshToken: refreshToken,
+        refreshToken
       },
       { new: true }
-    )
+    );
 
     res.cookie('refreshToken', refreshToken, {
       httpOnly: true, // means it cannot be accessed by client-side JavaScript
-      maxAge:72 * 60 * 60 * 1000
-    })
+      maxAge: 72 * 60 * 60 * 1000
+    });
     // If the password is correct, send a JSON response with user information and a token
     res.json({
       _id: findUser?._id,
@@ -75,98 +72,98 @@ const loginUserCtrl = asyncHandler(async (req, res) => {
       email: findUser?.email,
       mobile: findUser?.mobile,
       token: generateToken(findUser?._id)
-    })
+    });
   } else {
-     // If the password is incorrect or the user doesn't exist, throw an error
-    throw new Error('Invalid credentials')
+    // If the password is incorrect or the user doesn't exist, throw an error
+    throw new Error('Invalid credentials');
   }
-})
+});
 
 // Handle refresh token
 
 const handleRefreshToken = asyncHandler(async (req, res) => {
   // Extract refresh token from cookies
-  const cookie = req.cookies
-  console.log(cookie)
+  const cookie = req.cookies;
+  console.log(cookie);
 
   // Check if refresh token exists in cookies
   if (!cookie?.refreshToken) {
-    throw new Error('No refresh token in cookies')
+    throw new Error('No refresh token in cookies');
   }
 
-  const refreshToken = cookie?.refreshToken
-  console.log(refreshToken)
+  const refreshToken = cookie?.refreshToken;
+  console.log(refreshToken);
 
   // Find user based on refresh token
-  const user = await User.findOne({ refreshToken })
+  const user = await User.findOne({ refreshToken });
 
-  if(!user) throw new Error(' No Refresh token present in db or not matched')
-  
+  if (!user) throw new Error(' No Refresh token present in db or not matched');
+
   // Verify the refresh token
   jwt.verify(refreshToken, process.env.JWT_SECRET, (err, decoded) => {
     if (err || user.id !== decoded.id) {
-      throw new Error('There is something wrong with the refresh token')
+      throw new Error('There is something wrong with the refresh token');
     }
 
     // Generate a new access token
-    const accessToken = generateToken(user?._id)
-    res.json({ accessToken })
-  })
-})
+    const accessToken = generateToken(user?._id);
+    res.json({ accessToken });
+  });
+});
 
 // Logout functionality
 // Clears the refresh token for the authenticated user
 const logout = asyncHandler(async (req, res) => {
-  const cookie = req.cookies
+  const cookie = req.cookies;
 
   // Check if refresh token exists in cookies
-  if (!cookie?.refreshToken) throw new Error('No refresh token in Cookies')
+  if (!cookie?.refreshToken) throw new Error('No refresh token in Cookies');
 
-  const refreshToken = cookie.refreshToken
+  const refreshToken = cookie.refreshToken;
 
   // Find user by refresh token
-  const user = await User.findOne({refreshToken})
+  const user = await User.findOne({ refreshToken });
   if (!user) {
     // Clear the refresh token cookie and respond with forbidden status
     res.clearCookie('refreshToken', {
       httpOnly: true,
       secure: true
-    })
-    return res.sendStatus(204)  // forbidden
+    });
+    return res.sendStatus(204); // forbidden
   }
 
   // Update user's refresh token to an empty string
-  await User.findOneAndUpdate({refreshToken: refreshToken}, {
+  await User.findOneAndUpdate({ refreshToken }, {
     refreshToken: ''
-  })
+  });
 
   // Clear the refresh token cookie and respond with forbidden status
   res.clearCookie('refreshToken', {
-      httpOnly: true,
-      secure: true
-    })
-    res.sendStatus(204)  // forbidden
-})
+    httpOnly: true,
+    secure: true
+  });
+  res.sendStatus(204); // forbidden
+});
 
 // Update a user
 const updateaUser = asyncHandler(async (req, res) => {
-  console.log(req.user)
+  console.log(req.user);
   // const {id} = req.user
-  const { _id } = req.params
-  validateMongoId(_id)
+  const { _id } = req.params;
+  validateMongoId(_id);
   try {
     const updateUser = await User.findByIdAndUpdate(id, {
       firstname: req?.body?.firstname,
       lastname: req?.body?.lastname,
       email: req?.body?.email,
-      mobile: req?.body?.mobile,
-      
-    }, {new: true,} )
-    res.json(updateUser)
+      mobile: req?.body?.mobile
+
+    }, { new: true });
+    res.json(updateUser);
   } catch (error) {
-    throw new Error(error)
+    throw new Error(error);
   }
-})
+});
 
 /**
  * getallUsers - Route handler function to retrieve all users.
@@ -184,47 +181,46 @@ const getallUsers = asyncHandler(async (req, res) => {
   try {
     // Implement the logic to retrieve all users from the system
     // This might involve querying a database or fetching data
-    const getUsers = await User.find()
+    const getUsers = await User.find();
 
     // Send the retrieved users as the response
-    res.json(getUsers)
+    res.json(getUsers);
   } catch (err) {
-    throw new Error(error)
+    throw new Error(error);
   }
-})
+});
 
 // Get a single user
 const getaUser = asyncHandler(async (req, res) => {
   // console.log(req.params)
-  const { id } = req.params
+  const { id } = req.params;
   console.log('Received ObjectId:', id);
-  validateMongoId(id)
+  validateMongoId(id);
   try {
-    const getaUser = await User.findById(id)
-    res.json({getaUser})
+    const getaUser = await User.findById(id);
+    res.json({ getaUser });
   } catch (err) {
-    throw new Error(err)
+    throw new Error(err);
   }
-  
-})
+});
 
 // Delete a user
 const deleteaUser = asyncHandler(async (req, res) => {
-  console.log(req.params)
-  const { id } = req.params
-  validateMongoId(id)
+  console.log(req.params);
+  const { id } = req.params;
+  validateMongoId(id);
   try {
-    const getaUser = await User.findByIdAndDelete(id)
-    res.json({deleteaUser})
+    const getaUser = await User.findByIdAndDelete(id);
+    res.json({ deleteaUser });
   } catch (err) {
-    throw new Error(error)
+    throw new Error(error);
   }
-})
+});
 
 // Block user
 const blockUser = asyncHandler(async (req, res) => {
-  const {id} = req.params
-  validateMongoId(id)
+  const { id } = req.params;
+  validateMongoId(id);
   try {
     const block = await User.findByIdAndUpdate(
       id,
@@ -234,19 +230,19 @@ const blockUser = asyncHandler(async (req, res) => {
       {
         new: true
       }
-    )
+    );
     res.json({
-      message:'User blocked'
-    })
+      message: 'User blocked'
+    });
   } catch (error) {
-    throw new Error(error)
+    throw new Error(error);
   }
-})
+});
 
 // Unblock user
 const unblockUser = asyncHandler(async (req, res) => {
-  const {id} = req.params
-  validateMongoId(id)
+  const { id } = req.params;
+  validateMongoId(id);
   try {
     const unblock = await User.findByIdAndUpdate(
       id,
@@ -256,17 +252,30 @@ const unblockUser = asyncHandler(async (req, res) => {
       {
         new: true
       }
-    )
+    );
     res.json({
-      message:'User unblocked'
-    })
+      message: 'User unblocked'
+    });
   } catch (error) {
-    throw new Error(error)
+    throw new Error(error);
   }
+});
 
-})
+const updatePassword = asyncHandler(async (req, res) => {
+  const { _id } = req.user;
+  const {password} = req.body;
+  validateMongoId(_id);
+  const user = await User.findById(_id);
+  if (password) {
+    user.password = password;
+    const updatedPassword = await user.save();
+    res.json(updatedPassword);
+  } else {
+    res.json(user);
+  }
+});
 
-module.exports = { 
+module.exports = {
   createUser,
   loginUserCtrl,
   getallUsers,
@@ -276,5 +285,6 @@ module.exports = {
   blockUser,
   unblockUser,
   handleRefreshToken,
-  logout
+  logout,
+  updatePassword
 };
