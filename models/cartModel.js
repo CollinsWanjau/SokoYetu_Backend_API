1d3ad62e76e5860723e7f52d097a60a82da98dd9
colloso999 <tripperskripper@gmail.com>
2024-01-02 23:06:58 +0300

    Add cart model schema

---
+++ models/cartModel.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose'); // Erase if already required
+
+// Declare the Schema of the Mongo model
+var cartSchema = new mongoose.Schema(
+    {
+    // an array of objects, where each object represents a product in the order
+    // Each product has an ObjectId reference to a Product model (ref: 'Product'), 
+    // a count (which is presumably the quantity of the product in the order), and a color.
+    products: [
+        {
+            product: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'Product'
+            },
+            count: Number,
+            color: String,
+            price: Number
+        },
+    ],
+
+    // an object that will likely hold information about the payment intent for the order
+    paymentIntent: {},
+
+    // a string that represents the status of the order.
+    // it can only be one of the values in the provided enum array.
+    cartTotal: Number,
+    totalAfterDiscount: Number,
+    orderby: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+}, {
+    timestamps: true
+}
+);
+
+//Export the model
+module.exports = mongoose.model('Cart', cartSchema);
